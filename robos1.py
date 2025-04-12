d6a86f7a03412b0ef755c1deec6ea9af30c70f59
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 20:19:41 -0300

    Update robos1.py

--- robos1.py
+++ robos1.py
@@ -9,10 +9,17 @@ for robo in robos:
     cont = 1
     verdade = False
     if robo[0] < robo[cont]:
-        for i in range(1, len(robo)+1):
+        for i in range(1, len(robo)-1):
             cont += 1
+            if robo[-2] < robo[-1]:
+                verdade = False
+                break
             if robo[i] < robo[cont]:
                 verdade = True
+            elif robo[i] == robo[cont]:
+                verdade = False
+                break
+            
             else:
                 verdade = True
     if verdade == True:
