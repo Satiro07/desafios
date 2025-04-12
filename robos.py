25707d65466abb5dcfb10b663821ba7b7af64d53
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 19:30:15 -0300

    Update robos.py

--- robos.py
+++ robos.py
@@ -1,4 +1,3 @@
-from random import randint
 robos = list()
 quantidade_robos = int(input('Quantidade de robôs: '))
 for i in range(0, quantidade_robos):
@@ -6,3 +5,10 @@ for i in range(0, quantidade_robos):
     robos.append(sequencia)
 for i in robos:
     print(i)
+for i in robos:
+    if i == i[::-1]:
+        print('REBOLADO')
+    else:
+        print('NORMAL')
+    
+    
