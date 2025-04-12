5740e406d97394d6ec1a776b7af22dcc7e291f69
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 19:31:09 -0300

    Update robos.py

--- robos.py
+++ robos.py
@@ -4,8 +4,6 @@ for i in range(0, quantidade_robos):
     sequencia = input('').strip()
     robos.append(sequencia)
 for i in robos:
-    print(i)
-for i in robos:
     if i == i[::-1]:
         print('REBOLADO')
     else:
