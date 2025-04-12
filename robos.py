4c0a7806abf5a4658aa8ec6f85f66f372225f45b
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 19:41:49 -0300

    Update robos.py

--- robos.py
+++ robos.py
@@ -2,11 +2,11 @@ robos = list()
 quantidade_robos = int(input('Quantidade de robôs: '))
 for i in range(0, quantidade_robos):
     sequencia = input('').strip()
+    sequencia = list(map(int, sequencia.split()))
     robos.append(sequencia)
 for i in robos:
     if i == i[::-1]:
         print('REBOLADO')
     else:
         print('NORMAL')
-    
-    
+ 
