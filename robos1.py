9431f9e3d93bc9022be1e9287566cf32b28f4705
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 20:05:04 -0300

    Update robos1.py

--- robos1.py
+++ robos1.py
@@ -1,5 +1,15 @@
 
+robos = []
 quant_robos = int(input('Quantidade de robôs: '))
 for i in range(0, quant_robos):
     sequencia = input('Sequência: ')
     sequencia = list(map(int, sequencia.split()))
+    robos.append(sequencia)
+for robo in robos:
+    cont = 1
+    for i in range(0, len(robo)):
+        if robo[i] < robo[cont]:
+            pass
+        else:
+            print('NORMAL')
+
