7556ddb782a832ac51ec383f37c29e5df84920a2
Satiro07 <jardelsatiro57@gmail.com>
2025-04-12 19:29:55 -0300

    Update robos.py

--- robos.py
+++ robos.py
@@ -2,10 +2,7 @@ from random import randint
 robos = list()
 quantidade_robos = int(input('Quantidade de robôs: '))
 for i in range(0, quantidade_robos):
-    robo = []
-    quantidade_digs = randint(1, 10)
-    while quantidade_digs:
-        digito = randint(1, 9)
-        robo.append(digito)
-    robos.append([robo])
-print(robos)
+    sequencia = input('').strip()
+    robos.append(sequencia)
+for i in robos:
+    print(i)
