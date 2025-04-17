3e7aafa35f3243e93f9bb2ea93b4d18fe216d062
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 18:06:48 -0300

    Update matriz.py

--- matriz.py
+++ matriz.py
@@ -8,7 +8,7 @@ for i in range(0,linhas):
         add_col = int(input(f'Número na linha {i+1} e coluna {y+1}: '))
         col.append(add_col)
     matriz.append(col)
-
+print()
 for i in matriz:
     print(i)
 
@@ -16,15 +16,15 @@ print()
 print('Matriz transposta:')
 
 c = 0
-for i in matriz:
+for i in matriz: # conta quantas colunas tem na matriz
     for x in i:
         c += 1
     break
 mt = []
-for x in range(0, c):
+for x in range(0, c): # loop é feito de acordo com a quantidade de colunas
     mt1 = []
     cont = 0
-    for y in matriz:
+    for y in matriz: # loop é feito de acordo com a quantidade de linhas
         mt1.append(matriz[cont][x])
         cont += 1
     mt.append(mt1)
